b5ee11905e8415e7fcccdd4d94bf6a613d272637
sifad-hossain <niaz.sifad@gmail.com>
2024-03-19 23:48:30 +0600

    first commit

---
+++ assingment.js
@@ -0,0 +1,110 @@
+function calculateMoney(ticketSale) {
+    if (ticketSale < 0) {
+    return "please input a positve value";
+    }
+    const ticketPrice = 120;
+    const totalSell = ticketPrice * ticketSale;
+    const totalCost = 500 + 8 * 50;
+    const result = totalSell - totalCost;
+    return result;
+    }
+
+const calcu = calculateMoney(-20)
+console.log(calcu);
+
+
+
+
+
+
+
+
+    function checkName(name) {
+    if (typeof name !== "string") {
+    return "invalid";
+    }
+    const paramsValue = name.toLowerCase();
+    const checkName = paramsValue.split("");
+    const lastvalue = checkName[checkName.length - 1];
+    const values = ["a", " y", "i", "e", "o", "u", "w"];
+    for (const word of values) {
+    if (word === lastvalue) {
+    return "Good Name";
+    }
+    }
+    return "Bad Name";
+    }
+
+const man = checkName(122);
+console.log(man);
+
+
+
+
+
+    function deleteInvalids(array) {
+    if (Array.isArray(array) == false) {
+    return "please give an array";
+    }
+    let result = [];
+    for (const value of array) {
+    if (typeof value === "number" && !isNaN(value)) {
+    result.push(value);
+    }
+    }
+    return result;
+    }
+
+const delet = deleteInvalids({num: [ 1 , 2 , 3 ]});
+console.log(delet);
+
+
+
+
+function password(obj) {
+    if (!obj.siteName || !obj.birthYear || !obj.name) {
+      return "invalid";
+    }
+    if (obj.birthYear.toString().length != 4) {
+      return "invalid";
+    }
+
+    obj.siteName = obj.siteName[0].toUpperCase() + obj.siteName.slice(1);   
+    return obj.siteName + "#" + obj.name + "@" + obj.birthYear;
+  }
+
+
+
+ const pass = password({ name: "kolimuddin" , birthYear: 1999 , siteName: "google" })
+
+console.log(pass);
+
+
+
+
+
+
+    function monthlySavings(arr, livingCost) {
+    if (Array.isArray(arr) == false || typeof livingCost !== "number") {
+    return "invalid input";
+    }
+    let totalIncome = 0;
+    let IncreaseIncome = 0;
+    for (const value of arr) {
+    totalIncome += value;
+    if (value >= 3000) {
+    IncreaseIncome += value;
+    }
+    }
+    const taxvalue = (IncreaseIncome * 20) / 100;
+    const totalCost = livingCost + taxvalue;
+    const savingMoney = totalIncome - totalCost;
+    if (savingMoney < 0) {
+    return "earn more";
+    } else {
+    return savingMoney;
+    }
+    }
+
+const month = monthlySavings(100, [ 900 , 2700 , 3400])
+console.log(month);
